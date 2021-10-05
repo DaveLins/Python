4c07580455234f563437d9be00c0984b2e3cc432
David Lins <69007873+cyber969696@users.noreply.github.com>
2021-10-05 19:51:15 -0300

    Create Robozinho mouse.py

---
+++ Robozinho mouse.py
@@ -0,0 +1,19 @@
+import pyautogui
+
+from time import sleep
+
+pyautogui.click('C:\Files importantes\Python - Scripts\Pyautogui\Capturar.PNG')
+
+sleep(3)
+
+pyautogui.write('Bloco de notas')
+
+sleep(1)
+
+pyautogui.press('enter')
+
+sleep(1)
+
+pyautogui.write('Ola eu sou um robo!')
+
+sleep(1)
