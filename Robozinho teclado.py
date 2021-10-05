a4c76f6a03ca6697176fe35ab3a33a26f7c14984
David Lins <69007873+cyber969696@users.noreply.github.com>
2021-10-05 19:51:55 -0300

    Create Robozinho teclado.py

---
+++ Robozinho teclado.py
@@ -0,0 +1,36 @@
+import pyautogui
+
+from time import sleep
+
+def Firebase():
+
+    # Segure o windows
+    pyautogui.keyDown('win')
+
+    # Pressione o botão 1
+    pyautogui.press('1')
+
+    # Solte o windows
+    pyautogui.keyUp('win')
+
+    # Segure o ctrl
+    pyautogui.keyDown('ctrl')
+
+    # Pressione o t
+    pyautogui.press('t')
+
+    # Solte o ctrl
+    pyautogui.keyUp('ctrl')
+
+    # Espere 2 segundos
+    sleep(2)
+
+    # Escreva Python
+    pyautogui.write('Firebase')
+
+    # Pressione enter
+    pyautogui.press('enter')
+
+Firebase()
+
+
